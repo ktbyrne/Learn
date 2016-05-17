b9a1c75259405f2382873f4ff7854f0d3b579543
ktbyrne <ktbyrne@gmail.com>
2016-05-17 15:12:33 -0400

    thirdscript

--- LearnScript.R
+++ LearnScript.R
@@ -1,12 +1,12 @@
 library(flowAutomateR)
-Kinetics(DataPath = "PDA13Rkin.xlsx")
+Kinetics(DataPath = "")
 getwd()
 setwd()
 flowAutomateR:::.StopParallel()
 FlowGateRename(ID = "PDA")
-BatchFlowPlots(ATitle = "20150303_PDA")
-BatchFlowPlots(DataPath = "20150303_PDA_Orthotopic_Flow_II_and_III_tumor_redacted.xlsx", 
-               ATitle = "PDA_Treated_Orthotopic", YVarFactor = NULL, Statistics = TRUE, 
-               PValLim = 0.05, Maintitle = "PDA_Treated_Orthotopic", OutputSizeX = 6, 
+BatchFlowPlots(ATitle = "")
+BatchFlowPlots(DataPath = "", 
+               ATitle = "", YVarFactor = NULL, Statistics = TRUE, 
+               PValLim = 0.05, Maintitle = "", OutputSizeX = 6, 
                OutputSizeY = 9, Output_font_size = 100)
 
